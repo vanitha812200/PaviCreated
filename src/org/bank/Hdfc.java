d67976bc806c305ac17e6252704276966d7a6ac1
vanitha812200 <vanithamanthiribe1@gmail.com>
2023-01-05 13:04:09 +0530

    PaviGit

---
+++ src/org/bank/Hdfc.java
@@ -0,0 +1,9 @@
+package org.bank;
+
+class Hdfc extends WorldBank {
+public static void main(String[] args) {
+	Hdfc f=new Hdfc();
+	f.savings();
+}
+}
+
---
+++ src/org/bank/WorldBank.java
@@ -0,0 +1,10 @@
+package org.bank;
+
+public class WorldBank {
+public static void savings() {
+	System.out.println("saving");
+	
+
+}
+
+}
